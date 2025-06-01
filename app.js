d5befba81551bd18040aeff86b4341fa33ed47e6
michealdvlp <micheal@dvlp.app>
2025-06-01 17:17:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // API base URL - change to your local server address for testing
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://healthmatebackend-one.vercel.app/';
 
 // Update the checkSymptoms function to work with the new HTML structure
 async function checkSymptoms() {
