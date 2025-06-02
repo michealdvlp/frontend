fd530dbcccfc3cbad7138e643748fae56e838132
michealdvlp <micheal@dvlp.app>
2025-06-02 16:12:27 +0100

    Update awareness.js

--- awareness.js
+++ awareness.js
@@ -1,5 +1,5 @@
 // API base URL - change to your local server address for testing
-const API_BASE_URL = 'https://cjid-healthmate-ai-2onrender.com';
+const API_BASE_URL = 'https://healthmatebackendm.vercel.app/';
 
 // Cache management
 const CACHE_DURATION = 60 * 60 * 1000; // 1 hour in milliseconds
